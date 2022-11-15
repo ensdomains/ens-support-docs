d7e7eaf51c020df2a93d299e4625075417f2372f
Cthulu.eth <rasmus@ens.domains>
2022-11-15 17:50:17 +0100

    fix: reorganize code

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,18 +4,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import {HomepageInfoCardsRow} from '@site/src/components/HomepageInfo';
-import {HomepageTextLinks} from '@site/src/components/HomepageTextLinks';
 import styles from './index.module.css';
-
-/*
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-    </header>
-  );
-}
-*/
 
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
@@ -24,10 +13,9 @@ export default function Home() {
       title={`${siteConfig.title}`}
       description="ENS Support Docs">
       <main>
-    <HomepageTextLinks />
-    <section className={styles.body}>
-	  <HomepageInfoCardsRow />
-    </section>
+        <section className={styles.body}>
+          <HomepageInfoCardsRow />
+        </section>
       </main>
     </Layout>
   );
