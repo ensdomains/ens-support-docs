5f8c7363755ea9b71abd1d872accb2c72cff7cea
Cthulu.eth <rasmus@ens.domains>
2022-11-15 17:25:33 +0100

    fix: make discord navbar icon circular

---
+++ src/components/HomepageTextLinks/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css'
+
+export function HomepageTextLinks() {
+  return (
+    <header className={styles.textlinks}>
+    </header>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,42 +4,18 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import {HomepageInfoCardsRow} from '@site/src/components/HomepageInfo';
+import {HomepageTextLinks} from '@site/src/components/HomepageTextLinks';
 import styles from './index.module.css';
 
+/*
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-	<div class="docs-card-container">
-	<div class="row row-cols-1 row-cols-md-3a g-4">
-
-  	<div class="col">
-    	<div class="card card-body h-100 d-flex flex-column" >
-    		<a href="docs/intro" class="card-title card-link stretched-link"> <h2>Getting started</h2></a>
-    		<p style={{ color: 'black' }}>Get started with your ENS name</p>
-	</div>
-  </div>
-  <div class="col">
-	<div class="card card-body h-100 d-flex flex-column">
-    		<a href="blog/" class="card-title card-link stretched-link"> <h2>Support updates</h2></a>
-    <p style={{ color: 'black' }}>The latest support updates</p>
-</div>
-    </div>
-  <div class="col">
-	<div class="card card-body h-100 d-flex flex-column">
-    		<a href="docs/faq" class="card-title card-link stretched-link"> <h2>Frequently Asked Questions</h2></a>
-    <p style={{ color: 'black' }}>The most frequently asked questions</p>
-    </div>
-</div>
-    </div>
-    </div>
-        <div className={styles.buttons}>
-        </div>
-      </div>
     </header>
   );
 }
+*/
 
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
@@ -48,6 +24,7 @@ export default function Home() {
       title={`${siteConfig.title}`}
       description="ENS Support Docs">
       <main>
+    <HomepageTextLinks />
     <section className={styles.body}>
 	  <HomepageInfoCardsRow />
     </section>
