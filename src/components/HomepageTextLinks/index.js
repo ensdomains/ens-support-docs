80f8d940f5188ee88c00479a10fabab4041c7231
Cthulu.eth <rasmus@ens.domains>
2022-11-18 10:21:54 +0100

    fix: clean up css formatting

--- src/components/HomepageTextLinks/index.js
+++ src/components/HomepageTextLinks/index.js
@@ -5,21 +5,21 @@ import styles from './styles.module.css'
 export function HomepageTextLinks() {
   return (
     <header class={styles.header}>
-      <table class={styles.table}>
+      <table>
         <center>
-          <td class={styles.td}>
-          <tr class={styles.tr2}>🛈 Getting Started</tr>
+          <td>
+          <tr>🛈 Getting Started</tr>
           </td>
-          <td class={styles.td}>
-          <tr class={styles.tr2}>🖺 Tutorials</tr>
+          <td>
+          <tr>🗪 Frequently Asked Questions</tr>
           </td>
-          <td class={styles.td}>
-          <tr class={styles.tr2}>🗪 Frequently Asked Questions</tr>
+          <td>
+          <tr>🖺 Tutorials</tr>
           </td>
     </center>
         <tbody class={styles.tbody}>
-          <tr class={styles.tr}>
-            <td class={styles.td}>
+          <tr>
+            <td>
             <center>
               <a href="docs/intro">Introduction</a>
               <br></br>
@@ -28,22 +28,22 @@ export function HomepageTextLinks() {
               <a href="docs/names/avatar">The Avatar Record</a>
             </center>
             </td>
-            <td class={styles.td}>
-              <center>
-                <a href="docs/tutorials/registration">How to register an ENS name</a>
-                <br></br>
-                <a href="docs/tutorials/registration">How to transfer an ENS name</a>
-                <br></br>
-                <a href="docs/tutorials/registration">How to set an Avatar</a>
-              </center>
-            </td>
-            <td class={styles.td}>
+            <td>
               <center>
                 <a href="docs/faq/marketplaces/opensea#ens-name-doesnt-show">Why doesn't my name show on OpenSea?</a>
                 <br></br>
                 <a href="docs/faq/wallets/metamask#exaggerated-gas-costs">Why is the gas fee so high?</a>
                 <br></br>
                 <a href="docs/faq/wallets/metamask#exaggerated-gas-costs">Is this airdrop link real?</a>
+              </center>
+            </td>
+            <td>
+              <center>
+                <a href="docs/tutorials/registration">How to register an ENS name</a>
+                <br></br>
+                <a href="docs/tutorials/registration">How to transfer an ENS name</a>
+                <br></br>
+                <a href="docs/tutorials/registration">How to set an Avatar</a>
               </center>
             </td>
           </tr>
