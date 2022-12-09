0fd5bf5dc5458418c39aab676d24d14406b08372
cthulu.eth <rasmus@ens.domains>
2022-12-09 18:18:35 +0100

    add router redirect

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,24 +7,29 @@ import styles from './index.module.css';
 import {HomepageInfoCardsRow} from '@site/src/components/HomepageInfo';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import SearchBar from '@site/src/components/SearchBar';
+import {Redirect} from '@docusaurus/router';
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={`${siteConfig.title}`}
-      description="ENS Support Docs">
-      <main>
-        <section class={styles.features}>
-          <HomepageFeatures />
-          <SearchBar />
-        </section>
-        {/*<section className={styles.infoCards}>
-            <center>
-              <HomepageInfoCardsRow />
-            </center>
-            </section>*/}
-      </main>
-    </Layout>
-  );
-}
+	return <Redirect to="/docs" />;
+};
+
+// export default function Home() {
+//   const {siteConfig} = useDocusaurusContext();
+//   return (
+//     <Layout
+//       title={`${siteConfig.title}`}
+//       description="ENS Support Docs">
+//       <main>
+//         <section class={styles.features}>
+//           <HomepageFeatures />
+//           <SearchBar />
+//         </section>
+//         {/*<section className={styles.infoCards}>
+//             <center>
+//               <HomepageInfoCardsRow />
+//             </center>
+//             </section>*/}
+//       </main>
+//     </Layout>
+//   );
+// }
