917da3b75a6187eb5e38898715e8c9960922b878
cthulu.eth <rasmus@ens.domains>
2022-12-10 07:51:25 +0100

    code consolidation

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,24 +4,3 @@ import {Redirect} from '@docusaurus/router';
 export default function Home() {
 	return <Redirect to="/docs" />;
 };
-
-// export default function Home() {
-//   const {siteConfig} = useDocusaurusContext();
-//   return (
-//     <Layout
-//       title={`${siteConfig.title}`}
-//       description="ENS Support Docs">
-//       <main>
-//         <section class={styles.features}>
-//           <HomepageFeatures />
-//           <SearchBar />
-//         </section>
-//         {/*<section className={styles.infoCards}>
-//             <center>
-//               <HomepageInfoCardsRow />
-//             </center>
-//             </section>*/}
-//       </main>
-//     </Layout>
-//   );
-// }
