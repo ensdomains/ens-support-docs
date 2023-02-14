c87ad4f2fb7bf2694187f67fcc6e99beecab1fbe
cthulu.eth <rasmus@ens.domains>
2023-02-14 16:34:16 +0200

    Add new ImageWindow component

---
+++ src/components/ImageWindow/index.tsx
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React, {type ReactNode} from 'react';
+import clsx from 'clsx';
+
+import styles from './styles.module.css';
+
+interface Props {
+  children: ReactNode;
+  minHeight: number;
+  title: string;
+}
+
+export default function ImageWindow({
+  children,
+  minHeight,
+  title = 'image',
+}: Props): JSX.Element {
+  return (
+    <div className={styles.imageWindow} style={{minHeight}}>
+      <div className={styles.imageWindowHeader}>
+        <div className={clsx(styles.imageWindowAddressBar, 'text--truncate')}>
+          {title}
+        </div>
+      </div>
+
+      <div className={styles.imageWindowBody}>{children}</div>
+    </div>
+  );
+}
