5b89ac45fafca4904357b69be7a7de7bb67ab629
cthulu.eth <rasmus@ens.domains>
2023-02-17 16:45:12 +0200

    component and styling changes

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import MDXComponents from '@theme-original/MDXComponents';
 import BrowserWindow from '@site/src/components/BrowserWindow';
+import ImageWindow from '@site/src/components/ImageWindow';
 import Image from '@theme/IdealImage';
+import Tabs from '@theme/Tabs';
+import TabItem from '@theme/TabItem';
 import Link from '@docusaurus/Link';
 
 export default {
   ...MDXComponents,
   BrowserWindow: BrowserWindow,
+  ImageWindow: ImageWindow,
   Image: Image,
   Link: Link,
+  Tabs: Tabs,
+  TabItem: TabItem,
 };
