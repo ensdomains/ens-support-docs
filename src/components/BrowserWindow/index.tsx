c74cd69003b04a084a7dc9d813cb177f1c48e385
cthulu.eth <rasmus@ens.domains>
2023-03-24 12:24:33 +0200

    responsive design adjustments

--- src/components/BrowserWindow/index.tsx
+++ src/components/BrowserWindow/index.tsx
@@ -14,34 +14,40 @@ interface Props {
   children: ReactNode;
   minHeight: number;
   url: string;
+  padding: string;
 }
 
 export default function BrowserWindow({
   children,
   minHeight,
+  padding = '25px 0px 25px 0px',
   url = 'http://localhost:3000',
 }: Props): JSX.Element {
   return (
-    <div className={styles.browserWindow} style={{minHeight}}>
-      <div className={styles.browserWindowHeader}>
-        <div className={styles.buttons}>
-          <span className={styles.dot} style={{background: '#f25f58'}} />
-          <span className={styles.dot} style={{background: '#fbbe3c'}} />
-          <span className={styles.dot} style={{background: '#58cb42'}} />
-        </div>
-        <div className={clsx(styles.browserWindowAddressBar, 'text--truncate')}>
-          {url}
-        </div>
-        <div className={styles.browserWindowMenuIcon}>
-          <div>
-            <span className={styles.bar} />
-            <span className={styles.bar} />
-            <span className={styles.bar} />
+    <p align="center">
+      <div style={{'padding':padding}}>
+        <div className={styles.browserWindow} style={{minHeight}}>
+          <div className={styles.browserWindowHeader}>
+            <div className={styles.buttons}>
+              <span className={styles.dot} style={{background: '#f25f58'}} />
+              <span className={styles.dot} style={{background: '#fbbe3c'}} />
+              <span className={styles.dot} style={{background: '#58cb42'}} />
+            </div>
+            <div className={clsx(styles.browserWindowAddressBar, 'text--truncate')}>
+              {url}
+            </div>
+            <div className={styles.browserWindowMenuIcon}>
+              <div>
+                <span className={styles.bar} />
+                <span className={styles.bar} />
+                <span className={styles.bar} />
+              </div>
+            </div>
           </div>
+
+          <div className={styles.browserWindowBody}>{children}</div>
         </div>
       </div>
-
-      <div className={styles.browserWindowBody}>{children}</div>
-    </div>
+    </p>
   );
 }
